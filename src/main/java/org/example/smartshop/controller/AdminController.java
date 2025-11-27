289192d8ff6cbe846d79f67124b1722507a33020
ayuub <ayoubbenomar1230@gmail.com>
2025-11-27 12:27:48 +0100

    added end points for users in the admin

--- src/main/java/org/example/smartshop/controller/AdminController.java
+++ src/main/java/org/example/smartshop/controller/AdminController.java
@@ -10,6 +10,8 @@ import org.example.smartshop.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
@@ -18,28 +20,46 @@ public class AdminController {
     private final UserService userService;
     private final ProductService productService;
 
-    @PostMapping("/create-client")
-    public ResponseEntity<?> createClient(@RequestBody UserDto dto, HttpSession session) {
-
+    private void checkAdmin(HttpSession session) {
         Object roleObj = session.getAttribute("USER_ROLE");
         if (roleObj == null || roleObj != UserRole.ADMIN) {
-            return ResponseEntity.status(403).body("Forbidden: Admin only");
+            throw new RuntimeException("Forbidden: Admin only");
         }
+    }
 
+    // ----- User management -----
+    @PostMapping("/create-client")
+    public ResponseEntity<?> createClient(@RequestBody UserDto dto, HttpSession session) {
+        checkAdmin(session);
         UserDto created = userService.create(dto);
         return ResponseEntity.ok(created);
     }
 
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDto>> getAllUsers(HttpSession session){
+        checkAdmin(session);
+        return ResponseEntity.ok(userService.getAll());
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Integer id, HttpSession session){
+        checkAdmin(session);
+        return ResponseEntity.ok(userService.getById(id));
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable Integer id, HttpSession session){
+        checkAdmin(session);
+        userService.delete(id);
+    }
+
+
+    // ----- Product management -----
     @PostMapping("/create-product")
     public ResponseEntity<?> createProduct(@RequestBody ProductDto dto, HttpSession session){
-        Object roleObj = session.getAttribute("USER_ROLE");
-        if(roleObj == null || roleObj != UserRole.ADMIN){
-            return ResponseEntity.status(403).body("Forbidden: Admin only");
-        }
-
+        checkAdmin(session);
         ProductDto created = productService.create(dto);
         return ResponseEntity.ok(created);
     }
-
 
 }
